a7addb73e10032c4eadefbd61591139c6d5c7d14
Jack <jack-nicol1@hotmail.co.uk>
2021-07-04 23:26:02 +0100

    Change return value

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 @app.route('/node/status')
 def node_status():
-    hostname = request.args.get('hostname')
-    response = os.system("ping -c 1 " + hostname)
+    ip_address = request.args.get('ip_address')
+    response = os.system("ping -c 1 " + str(ip_address))
     if response == 0:
-        status = json.loads('{ "status": "up" }')
+        status = 'up'
     else:
         print(response)
-        status = json.loads('{ "status": "down" }')
+        status = 'down'
 
     return status
